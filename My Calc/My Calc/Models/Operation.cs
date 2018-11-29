9cca34b91bf10533a56c53c25286b722df7b16ae
Anton Armaginov <armaginov@mail.ru>
2018-11-30 00:07:01 +0400

    task2

---
+++ My Calc/My Calc/Models/Operation.cs
@@ -0,0 +1,18 @@
+﻿using My_Calc.Resources;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace My_Calc.Models
+{
+    public enum Operation
+    {
+        [Display(Name = "Add", ResourceType = typeof(CalcResources))]
+        Add,
+        [Display(Name = "Diff", ResourceType = typeof(CalcResources))]
+        Diff,
+        [Display(Name = "Multiply", ResourceType = typeof(CalcResources))]
+        Multiply,
+        [Display(Name = "Share", ResourceType = typeof(CalcResources))]
+        Share
+    }
+}
